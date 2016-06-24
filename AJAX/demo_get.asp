df35af55c0d888674da38a39fbc89a52ae22125d
Vu Hoang Minh <minhmanutd@gmail.com>
2016-06-24 16:51:57 +0700

    asdkha

---
+++ AJAX/demo_get.asp
@@ -0,0 +1,2 @@
+This content was requested using the GET method.
+Requested at: 6/24/2016 12:27:50 AM
--- Using UI Router/app.js
+++ Using UI Router/app.js
@@ -54,3 +54,23 @@ angular.module('formApp', ['ngAnimate', 'ui.router'])
     };
     
 });
+
+//// send 
+//// =============================================================================
+//function loadDoc() {
+//  var xhttp;
+//  if (window.XMLHttpRequest) {
+//    // code for modern browsers
+//    xhttp = new XMLHttpRequest();
+//    } else {
+//    // code for IE6, IE5
+//    xhttp = new ActiveXObject("Microsoft.XMLHTTP");
+//  }
+//  xhttp.onreadystatechange = function() {
+//    if (xhttp.readyState == 4 && xhttp.status == 200) {
+//      document.getElementById("demo").innerHTML = xhttp.responseText;
+//    }
+//  };
+//  xhttp.open("GET", "ajax_info.txt", true);
+//  xhttp.send();
+//}
