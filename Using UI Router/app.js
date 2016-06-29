c371f3d03e107b4d85ed061faeec4f660c1e1f32
Vu Hoang Minh <minhmanutd@gmail.com>
2016-06-29 19:55:15 +0700

    s

--- Using UI Router/app.js
+++ Using UI Router/app.js
@@ -51,8 +51,8 @@ angular.module('formApp', ['ngAnimate', 'ui.router'])
     // function to process the form
     $scope.processForm = function() {
         
-        $http.jsonp('http://www.example.com?callback=JSON_CALLBACK')
-        .success (function('{"id":"3","description":"xx","floor":2}') {})
+//        $http.jsonp('http://www.example.com?callback=JSON_CALLBACK')
+//        .success (function('{"id":"3","description":"xx","floor":2}') {})
         
 //        var xhttp = new XMLHttpRequest();
 //        xhttp.open("POST", "http://192.168.1.12:8080/smartdatics-sora/restapi/room", false);
@@ -60,9 +60,18 @@ angular.module('formApp', ['ngAnimate', 'ui.router'])
 //        xhttp.send('{"id":"3","description":"xx","floor":2}');
 //        alert(parseJson(xhttp.responseText).mainMessage);
 //        console.log(xhttp.status);
+        
     };
 });
 
+        var MainCtrl = function($scope, $http) {
+        $scope.loading = true;
+        $http.post('http://192.168.1.14:8080/smartdatics-sora/restapi/room/', { id:"3",description:"hehe",floor:2 }).success(function(response) {
+                $scope.response = response;
+                $scope.loading = false;
+                alert("awesome");
+            });
+        };
 //    function parseJson(str) {
 //        try {
 //		  return JSON.parse(str);
@@ -85,3 +94,59 @@ angular.module('formApp', ['ngAnimate', 'ui.router'])
 //  .success (function(data) {
 //    data='{"id":"3","description":"xx","floor":2}';
 //  })
+
+
+
+
+
+
+// Your app's root module...
+angular.module('MyModule', [], function($httpProvider) {
+  // Use x-www-form-urlencoded Content-Type
+  $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
+
+  /**
+   * The workhorse; converts an object to x-www-form-urlencoded serialization.
+   * @param {Object} obj
+   * @return {String}
+   */ 
+  var param = function(obj) {
+    var query = '', name, value, fullSubName, subName, subValue, innerObj, i;
+      
+    for(name in obj) {
+      value = obj[name];
+        
+      if(value instanceof Array) {
+        for(i=0; i<value.length; ++i) {
+          subValue = value[i];
+          fullSubName = name + '[' + i + ']';
+          innerObj = {};
+          innerObj[fullSubName] = subValue;
+          query += param(innerObj) + '&';
+        }
+      }
+      else if(value instanceof Object) {
+        for(subName in value) {
+          subValue = value[subName];
+          fullSubName = name + '[' + subName + ']';
+          innerObj = {};
+          innerObj[fullSubName] = subValue;
+          query += param(innerObj) + '&';
+        }
+      }
+      else if(value !== undefined && value !== null)
+        query += encodeURIComponent(name) + '=' + encodeURIComponent(value) + '&';
+    }
+      
+    return query.length ? query.substr(0, query.length - 1) : query;
+  };
+
+  // Override $http service's default transformRequest
+  $httpProvider.defaults.transformRequest = [function(data) {
+    return angular.isObject(data) && String(data) !== '[object File]' ? param(data) : data;
+  }];
+});
+
+
+
+
