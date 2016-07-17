e0dac03b9c12f66d4a7cd8b78c50823472d6788c
vuhoangminh <minhmanutd@gmail.com>
2016-07-17 16:49:01 +0700

    ,mansdlahsfiola

--- AngularjsWeb/js/custom.js
+++ AngularjsWeb/js/custom.js
@@ -82,10 +82,163 @@ $(document).ready(function () {
 
 
 
+// $(document).ready(function() {
+	
+	// $('#table-recentsession').DataTable( {
+        // "paging":   false,
+        // "info":     false,
+		 // "columns": [
+			// { "width": "18%" },
+			// { "width": "18%" },
+			// { "width": "18%" },
+			// { "width": "18%" },
+			// { "width": "16%" },
+			// { "width": "16%" }
+		// ],
+		// dom: 'Bfrtip',
+		// buttons: {
+			// buttons: [ 'copy', 'excel', 'pdf', 'print' ]
+		// },
+		// // rowsGroup: [
+			// // 'second:name',
+			// // 0,
+			// // 2
+		// // ],
+    // } );
+	
+    // $('#table-database').DataTable( {
+        // "paging":   false,
+        // "info":     false,
+		 // "columns": [
+			// { "width": "18%" },
+			// { "width": "18%" },
+			// { "width": "18%" },
+			// { "width": "18%" },
+			// { "width": "16%" },
+			// { "width": "16%" }
+		// ],
+		// dom: 'Bfrtip',
+		// buttons: {
+			// buttons: [ 'copy', 'excel', 'pdf', 'print' ]
+		// },
+		// // rowsGroup: [
+			// // 'second:name',
+			// // 0,
+			// // 2
+		// // ],
+    // } );
+
+
+
+    // $('#table-loaddatabase').DataTable( {
+        // "paging":   false,
+        // "info":     false,
+		// "bFilter": false,
+		// "ordering": false,
+		 // "columns": [
+			// { "width": "16%" },
+			// { "width": "27%" },
+			// { "width": "27%%" },
+			// { "width": "15%" },
+			// { "width": "15%" }
+		  // ]		
+    // } );
+
+
+    // $('#table-parameter-single').DataTable( {
+        // "paging":   false,
+        // "info":     false,
+		// "bFilter": false,
+		// "ordering": false,
+		 // "columns": [
+			// { "width": "5%" },
+			// { "width": "19%" },
+			// { "width": "19%" },
+			// { "width": "19%" },
+			// { "width": "19%" },
+			// { "width": "19%" }
+		  // ]		
+    // } );
+
+    // $('#table-parameter-double').DataTable( {
+        // "paging":   false,
+        // "info":     false,
+		// "bFilter": false,
+		// "ordering": false,
+		 // "columns": [
+			// { "width": "5%" },
+			// { "width": "19%" },
+			// { "width": "19%" },
+			// { "width": "19%" },
+			// { "width": "19%" },
+			// { "width": "19%" }
+		  // ]		
+    // } );
+
+// } );
+
+
+
+
 $(document).ready(function() {
-    $('#table-database').DataTable( {
+	// var width = document.getElementById('foo').offsetWidth;
+	var table=$('#table-recentsession').DataTable( {
         "paging":   false,
         "info":     false,
+		"bDestroy": true,
+		 "columns": [
+			{ "width": "18%" },
+			{ "width": "18%" },
+			{ "width": "18%" },
+			{ "width": "18%" },
+			{ "width": "16%" },
+			{ "width": "16%" }
+		],
+		dom: 'Bfrtip',
+		buttons: {
+			buttons: [ 'copy', 'excel', 'pdf', 'print' ]
+		},
+
+		// fixedHeader: {
+            // header: true,
+            // footer: true
+        // },
+		
+		"sScrollY": "300px",
+        // "iDisplayLength": 25,
+        // "bJQueryUI": true,
+        // "bStateSave": true
+    } );
+
+	$('#table-recentsession tbody').on( 'click', 'tr', function () {
+        if ( $(this).hasClass('selected') ) {
+            $(this).removeClass('selected');
+        }
+        else {
+            table.$('tr.selected').removeClass('selected');
+            $(this).addClass('selected');
+        }
+    } );
+	
+		
+	$('#table-recentsession').DataTable().columns.adjust().draw();
+ 
+    $('#button').click( function () {
+        table.row('.selected').remove().draw( false );
+    } );
+	
+	// Customize search box here
+	$('#myInputTextField').keyup(function(){
+      table.search($(this).val()).draw() ;
+})
+} );
+
+
+$(document).ready(function() {
+    var table=$('#table-database').DataTable( {
+        "paging":   false,
+        "info":     false,
+		"bDestroy": true,
 		 "columns": [
 			{ "width": "18%" },
 			{ "width": "18%" },
@@ -104,14 +257,29 @@ $(document).ready(function() {
 			// 2
 		// ],
     } );
+	
+	$('#table-database tbody').on( 'click', 'tr', function () {
+        if ( $(this).hasClass('selected') ) {
+            $(this).removeClass('selected');
+        }
+        else {
+            table.$('tr.selected').removeClass('selected');
+            $(this).addClass('selected');
+        }
+    } );
+ 
+    $('#button').click( function () {
+        table.row('.selected').remove().draw( false );
+    } );
+} );
 
-
-
+$(document).ready(function() {
     $('#table-loaddatabase').DataTable( {
         "paging":   false,
         "info":     false,
 		"bFilter": false,
 		"ordering": false,
+		"bDestroy": true,
 		 "columns": [
 			{ "width": "16%" },
 			{ "width": "27%" },
@@ -120,36 +288,4 @@ $(document).ready(function() {
 			{ "width": "15%" }
 		  ]		
     } );
-
-
-    $('#table-parameter-single').DataTable( {
-        "paging":   false,
-        "info":     false,
-		"bFilter": false,
-		"ordering": false,
-		 "columns": [
-			{ "width": "5%" },
-			{ "width": "19%" },
-			{ "width": "19%" },
-			{ "width": "19%" },
-			{ "width": "19%" },
-			{ "width": "19%" }
-		  ]		
-    } );
-
-    $('#table-parameter-double').DataTable( {
-        "paging":   false,
-        "info":     false,
-		"bFilter": false,
-		"ordering": false,
-		 "columns": [
-			{ "width": "5%" },
-			{ "width": "19%" },
-			{ "width": "19%" },
-			{ "width": "19%" },
-			{ "width": "19%" },
-			{ "width": "19%" }
-		  ]		
-    } );
-
-} );
+} );	
